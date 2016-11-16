8a1a57efc14738f7649602e50edf0748f39945fc
Andrey Zhu <dir01@dir01.org>
2016-11-16 17:02:07 +0300

    Messages are not sent directly to users anymore. Everything is a room!

--- index.js
+++ index.js
@@ -9,19 +9,52 @@ var config = {
 };
 
 var io = require('socket.io')(config.port);
-var ioSocketId = 0;
-var ioSockets = {};
 var redis = require('redis');
-var redisReceivers = [];
 var redisPublishers = [];
 
 startSocketServer();
 connectToRedis(config.redis.urls);
 
-function startSocketServer () {
-    process.on('uncaughtException', onUncaughtException);
-    io.on('connection', onSocketConnection);
+
+function onSocketMessage (socket, message) {
+    log('Message from user received:', message);
+    if (!message.room) {
+        return;
+    }
+    message.socketId = socket.id;
+    message.ip = socket.handshake.headers['x-forwarded-for'] || socket.conn.remoteAddress;
+    publishRedisMessage('socket.io:auth', message);
 }
+
+
+function onRedisMessage (url, channel, message) {
+    log('Message from redis received:', message);
+    try {
+        var json = JSON.parse(message);
+        if (json.type === 'authorize') {
+            var socket = io.sockets.connected[json.socketId];
+            if (socket && json.room) {
+                log('Joining socket ' + json.socketId + ' to room ' + json.room);
+                socket.join(json.room);
+            }
+        } else {
+            io.to(json.room).emit(json.type, json);
+            log('Message ' + json.type + ' emitted to room ' + json.room + ':', message);
+        }
+    } catch (error) {
+        log('Error occured while parsing the message from redis:', message, error);
+    }
+}
+
+
+function startSocketServer() {
+    process.on('uncaughtException', onUncaughtException);
+    io.on('connection', function (socket) {
+        log('New connection is established');
+        socket.on('subscribe', onSocketMessage.bind(this, socket));
+    });
+}
+
 
 function connectToRedis (urls) {
     urls.forEach(function (url) {
@@ -31,8 +64,6 @@ function connectToRedis (urls) {
         receiver.on('message', onRedisMessage.bind(this, url));
         receiver.on('error', onRedisError.bind(this, url));
         receiver.on('end', onRedisEnd.bind(this, url));
-
-        redisReceivers.push(receiver);
         redisPublishers.push(redis.createClient(url));
     });
 }
@@ -49,42 +80,6 @@ function publishRedisMessage (channel, message) {
     });
 }
 
-function log () {
-    config.debug && console.log.apply(console, arguments);
-}
-
-function onRedisMessage (url, channel, message) {
-    log('Message from redis received:', message);
-    try {
-        var json = JSON.parse(message);
-        if (json.uid) {
-            var sockets = ioSockets[json.uid] || [];
-            if (!sockets.length) {
-                log('Sockets for UID ' + json.uid + ' do not exist');
-                return;
-            }
-
-            sockets.forEach(function (socket) {
-                if (json.type === 'authorize') {
-                    if (socket.id !== json.socketId) {
-                        return;
-                    }
-
-                    socket.authorized = true;
-                    log('User ' + json.uid + ' was authorized');
-                } else if (socket.authorized) {
-                    socket.emit(json.type, json);
-                    log('Message ' + json.type + ' sent to user ' + json.uid + ':', message);
-                }
-            });
-        } else {
-            io.to(json.room).emit(json.type, json);
-            log('Message ' + json.type + ' emitted to room ' + json.room + ':', message);
-        }
-    } catch (error) {
-        log('Error occured while parsing the message from redis:', message);
-    }
-}
 
 function onRedisError (url, error) {
     log('Error occured at ' + url + ':', error);
@@ -94,48 +89,11 @@ function onRedisEnd (url) {
     log('Redis connection closed at ' + url);
 }
 
-function onSocketConnection (socket) {
-    log('New connection is established');
-    socket.on('subscribe', onSocketMessage.bind(this, socket));
-    socket.on('disconnect', onSocketDisconnect.bind(this, socket));
-}
-
-function onSocketMessage (socket, message) {
-    log('Message from user received:', message);
-
-    if (!message.room && !message.uid) {
-        return;
-    }
-
-    if (message.uid) {
-        socket.uid = message.uid;
-        socket.id = ++ioSocketId;
-        ioSockets[message.uid] = ioSockets[message.uid] || [];
-        ioSockets[message.uid].push(socket);
-        message.socketId = socket.id;
-        message.ip = socket.handshake.headers['x-forwarded-for'] || socket.conn.remoteAddress;
-        publishRedisMessage('socket.io:auth', message);
-    } else {
-        log('User has joined to the room ' + message.room);
-        socket.join(message.room);
-    }
-}
-
-function onSocketDisconnect (socket) {
-    var uid = socket.uid;
-    if (!uid || !ioSockets[uid]) {
-        return;
-    }
-
-    var socketIndex = ioSockets[uid].indexOf(socket);
-    if (socketIndex === -1) {
-        return;
-    }
-
-    ioSockets[uid].splice(socketIndex, 1);
-    log('Socket with UID ' + uid + ' died');
-}
 
 function onUncaughtException (error) {
     log('Uncaught exception occured:', error);
+}
+
+function log () {
+    config.debug && console.log.apply(console, arguments);
 }
